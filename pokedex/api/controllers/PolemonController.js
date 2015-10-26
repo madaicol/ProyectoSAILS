d6c2a31b6f0fa70c36b4d5f115ce94af393abc08
Madai Arteaga <artmadai@hotmail.com>
2015-10-26 13:40:04 -0500

    Crear tabla Pokemon
    sails generate api Pokemon

---
+++ pokedex/api/controllers/PolemonController.js
@@ -0,0 +1,11 @@
+/**
+ * PolemonController
+ *
+ * @description :: Server-side logic for managing Polemons
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
